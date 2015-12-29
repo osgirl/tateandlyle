50e09b8bfcf1450b24fb39f024ee1b7ba7a4b9f4
Bogdan Stamate <bogdan.stamate@softescu.ro>
2015-12-29 14:39:57 +0200

    TL-48: Update - remove js (used for testing).

--- themes/custom/generic_microsite/js/custom.js
+++
@@ -1,7 +0,0 @@
-(function($){
-  $(document).ready(function () {
-    $('.popup').addClass('modal fade').attr('id', 'myModal');
-    $('.popup .paragraph').addClass('modal-content');
-    $('.imagine').attr('data-toggle', 'modal').attr('data-target', '#myModal');
-  });
-})(jQuery);
