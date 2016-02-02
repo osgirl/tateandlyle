774235d2c65bf77ee13e8f0712971e629d18c7d9
Balazs Dianiska <balazs.dianiska@acquia.com>
2016-02-02 10:40:34 +0100

    TL-137: Fixing the include paths.

--- simplesamlphp/www/_include.php
+++ simplesamlphp/www/_include.php
@@ -23,8 +23,8 @@ if (get_magic_quotes_runtime()) {
 
 
 /* Initialize the autoloader. */
-//require_once(dirname(dirname(__FILE__)) . '/lib/_autoload.php');
-require_once('/var/www/html/tatelyle.dev/simplesamlphp/lib/_autoload.php');
+require_once(dirname(dirname(__FILE__)) . '/lib/_autoload.php');
+//require_once('/var/www/html/tatelyle.dev/simplesamlphp/lib/_autoload.php');
 
 $path_extra = '/home/andreas/simplesamlphp/lib';
 
@@ -100,8 +100,8 @@ class SimpleSAML_IncPrefixWarn {
 $SIMPLESAML_INCPREFIX = new SimpleSAML_IncPrefixWarn();
 
 
-//$configdir = dirname(dirname(__FILE__)) . '/config';
-$configdir = '//var/www/html/tatelyle.dev/simplesamlphp/config';
+$configdir = dirname(dirname(__FILE__)) . '/config';
+//$configdir = '//var/www/html/tatelyle.dev/simplesamlphp/config';
 if (!file_exists($configdir . '/config.php')) {
 	header('Content-Type: text/plain');
 	echo("You have not yet created the simpleSAMLphp configuration files.\n");
