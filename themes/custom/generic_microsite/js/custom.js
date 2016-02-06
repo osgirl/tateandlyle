9fe5d963352d989d1dd2bd2b050a24ed23a2e7e9
Alex Tulbure <alex.tulbure@softescu.com>
2016-02-06 17:04:58 +0200

    TL 48  paragraph video on generic microsite theme

--- themes/custom/generic_microsite/js/custom.js
+++ themes/custom/generic_microsite/js/custom.js
@@ -91,4 +91,23 @@
     }
   }
 
+    //Modal video
+  Drupal.behaviors.modalVideo = {
+    attach: function (context, settings) {
+      var trigger = $("body").find('[data-toggle="modal"]');
+      trigger.click(function () {
+        var theModal = $(this).data("target"),
+        videoSRC = $(this).attr("data-theVideo"),
+        videoSRCauto = videoSRC + "?autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+          $(theModal + ' iframe').attr('src', videoSRC);
+        });
+        $('.modal').click(function () {
+          $(theModal + ' iframe').attr('src', videoSRC);
+        });
+      });
+    }
+  }
+
 })(jQuery);
