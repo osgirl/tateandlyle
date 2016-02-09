23f899d0467b2906e46b9f09bef9372074ec537a
Balazs Dianiska <balazs.dianiska@acquia.com>
2016-02-09 14:03:22 +0100

    TL-137: Wrapper script to support dashed domain names.

--- scripts/maintenance/drush-wrapper.sh
+++ scripts/maintenance/drush-wrapper.sh
@@ -5,7 +5,8 @@ env="dev"
 dblist=`aht @tatelyle.dev dbs | awk 'NR >=3 {print l} {l=$1}'`;
 
 for db in $dblist; do
-    site=`echo $db | tr -d '\n' | xargs`;
+    site=`echo $db | tr '_' '-' | tr -d '\n' | xargs`;
+    # Convert underscores to dash (e.g. soda-lo).
     if [ $site == "" ]; then continue; fi;
     domain="$site.$env.cloud.tateandlyle.com";
     aht @tatelyle.$env drush8 @tatelyle.$env -l $domain $@;
