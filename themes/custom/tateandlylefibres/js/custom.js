3fe3d3b1f5767e8e8fdf93cc53dd9a4b5c14187b
Bogdan Stamate <bogdan.stamate@softescu.ro>
2016-02-13 13:47:09 +0200

    TL-139: Revert calc for form page height.

--- themes/custom/tateandlylefibres/js/custom.js
+++ themes/custom/tateandlylefibres/js/custom.js
@@ -5,7 +5,6 @@
   // Unwrap html tags.
   Drupal.behaviors.overrideHTML = {
     attach: function (context, settings) {    
-
       $(".products .tout-container").click(function() {
         window.location = $(this).find("a").attr("href"); 
         return false;
@@ -17,24 +16,29 @@
       });
 
       if($('.is-form').length) {
-        $height_form = $('.field--name-field-form').height() + $('.field--name-field-form-header').height() + 222;
-        $('.field--name-field-main-content').css('height', $height_form )
+        $height_form = $('.field--name-field-form')[0].getBoundingClientRect().height + $('.field--name-field-form-header')[0].getBoundingClientRect().height;
+        $height_form_2 = $height_form - $('.field--name-field-form-header').innerHeight();
+        $('.field--name-field-form').css('height', $height_form_2);
+        $('.field--name-field-main-content').css('height', $height_form);
       }
       // Resize window
        $(window).resize(function(){
          if($('.is-form').length) {
-           $height_form = $('.field--name-field-form').height() + $('.field--name-field-form-header').height() + 218;
+        $height_form = $('.field--name-field-form')[0].getBoundingClientRect().height + $('.field--name-field-form-header')[0].getBoundingClientRect().height;
+        $height_form_2 = $height_form - $('.field--name-field-form-header').innerHeight();
+        $('.field--name-field-form').css('height', $height_form_2);
            $('.field--name-field-main-content').css('height', $height_form )
          }
       })
-    //$('.field--name-field-title').html($('.field--name-field-title').text().replace('®', '<sup>®</sup>'));
+
       $('.field--name-field-title, h1, h2, h3, a, p ').each(function(i, elem) {
-            $(elem).html(function(i, html) {
-                return html.replace('®', "<sup>&reg;</sup>");
-            });
+        $(elem).html(function(i, html) {
+            return html.replace('Â®', "<sup>&reg;</sup>");
         });
+      });
     }
   };
+
 
   //Modal video
   Drupal.behaviors.modalVideo = {
