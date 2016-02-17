017b71a5ede0ba283850b311067368f41876b464
Claudiu <claudiu.paulet@softescu.ro>
2016-02-17 16:14:13 +0200

    TL-02: Config install + updates.

--- modules/custom/tl_tweaks/src/Form/ContactFormSettingsForm.php
+++ modules/custom/tl_tweaks/src/Form/ContactFormSettingsForm.php
@@ -24,15 +24,16 @@ class ContactFormSettingsForm extends FormBase {
    * Custom settings form for tl_tweaks.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = \Drupal::configFactory()->get('tl_tweaks.settings');
     foreach (entity_get_bundles()['contact_message'] as $key => $contact_message) {
       if ($contact_message['label'] != 'Personal contact form') {
         $form['style'][$key] = array(
           '#type' => 'textfield',
           '#title' => t("@form form submit button text.", array('@form' => $contact_message['label'])),
-          '#default_value' => empty(\Drupal::configFactory()->get('tl_tweaks.settings')->get($key)) ? 'Submit' : \Drupal::configFactory()->get('tl_tweaks.settings')->get($key),
+          '#default_value' => $config->get($key),
           '#size' => 75,
           '#maxlength' => 75,
-          '#description' => t('Specify the submit button text for the @form form.', array('@form' => $contact_message['label'])),
+          '#description' => $this->t('Specify the submit button text for the @form form.', array('@form' => $contact_message['label'])),
         );
       }
     }
