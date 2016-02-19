2fc00aeb0efcf32e7cdebc8a29e3d9abe4dc08ed
Balazs Dianiska <balazs.dianiska@acquia.com>
2016-02-19 09:36:13 +0100

    TL-137: Environment as a variable and verbose command execution information in the wrapper script.

--- scripts/maintenance/drush-wrapper.sh
+++ scripts/maintenance/drush-wrapper.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-env="dev"
+env="test"
 
-dblist=`aht @tatelyle.dev dbs | awk 'NR >=3 {print l} {l=$1}'`;
+dblist=`aht @tatelyle.$env dbs | awk 'NR >=3 {print l} {l=$1}'`;
 
 for db in $dblist; do
     site=`echo $db | tr '_' '-' | tr -d '\n' | xargs`;
     # Convert underscores to dash (e.g. soda-lo).
     if [ $site == "" ]; then continue; fi;
     domain="$site.$env.cloud.tateandlyle.com";
+    echo "Running drush $@ on $domain";
     aht @tatelyle.$env drush8 @tatelyle.$env -l $domain $@;
 done
