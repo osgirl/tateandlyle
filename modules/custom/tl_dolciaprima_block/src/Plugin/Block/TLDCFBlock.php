be410561d26f6858b933e57ed25912352c726755
Bogdan Stamate <bogdanstamate@Bogdans-Mac-mini.local>
2016-03-21 15:49:10 +0200

    TL-151: Add custom Thank you message on Dolcia Prima form. Theming fixes.

--- modules/custom/tl_dolciaprima_block/src/Plugin/Block/TLDCFBlock.php
+++ modules/custom/tl_dolciaprima_block/src/Plugin/Block/TLDCFBlock.php
@@ -30,8 +30,9 @@ class TLDCFBlock extends BlockBase {
       ->create(array(
         'contact_form' => 'marketing',
       ));
-
     $form = \Drupal::service('entity.form_builder')->getForm($message);
+    $form['#prefix'] = '<div class="header-text">' . \Drupal::config('tl_dolciaprima_block.settings')->get('header_text') . '</div>';
+    $form['#suffix'] = '<a href="#" class="btn-close-form-submit-thank-you-message btn-close-form hide-text">Close</a><div class="form-submit-thank-you-message">' . \Drupal::config('tl_dolciaprima_block.settings')->get('submit_thank_you_message') . '</div>';
     return array(
       '#markup' => drupal_render($form),
     );
