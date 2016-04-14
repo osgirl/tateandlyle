ef6c016a559995b33c5d83849ae224a2f53eba08
Claudiu <claudiu.paulet@softescu.ro>
2016-04-14 14:41:54 +0300

    TL-029: Options preview block.

---
+++ modules/custom/tl_options_preview_block/src/Plugin/Block/TLOPBlock.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ * A custom block for Tate & Lyle Options Preview functionality.
+ */
+
+namespace Drupal\tl_options_preview_block\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Component\Utility\Html;
+
+/**
+ * Provides a custom block with the T&L Options Preview functionality.
+ *
+ * @Block(
+ *   id = "tl_options_preview_block",
+ *   admin_label = @Translation("Tate & Lyle Options Preview Block"),
+ *   category = @Translation("Blocks")
+ * )
+ */
+class TLOPBlock extends BlockBase {
+  /**
+   * Provides block content.
+   */
+  public function build() {
+    if ($_POST['field_identifier']) {
+      $content = "<ul>";
+      foreach ($_POST['field_identifier'] as $id) {
+        $option = explode(" | ", Html::escape($id));
+        $content .= "<li>" . $option[1] . "</li>";
+      }
+      $content .= "</ul>";
+    }
+    return [
+      '#theme' => 'tl_options_preview_block',
+      '#title' => [],
+      '#content' => $content,
+      '#tab' => Html::escape($_GET['tab']),
+      '#attributes' => [],
+    ];
+  }
+
+}
