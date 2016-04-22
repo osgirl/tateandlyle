ba20494833341effbb7f5ff5b0d8a8800af155c6
Balazs Dianiska <balazs.dianiska@acquia.com>
2016-04-22 12:28:14 +0200

    TL-137: Fixing domain name in the multisite parameter, ensuring it is passed to both the password reset and the database udpate scripts.

--- hooks/dev/post-db-copy/modules.sh
+++ hooks/dev/post-db-copy/modules.sh
@@ -13,7 +13,7 @@ domain_prefix=`echo $db_name | tr '_' '-'`
 
 site_name=$site.$target_env
 
-multisite="--uri=${domain_prefix}.${target_env}.tateandlyle.cloud.com --root=/var/www/html/${site_name}/docroot"
+multisite="--uri=${domain_prefix}.${target_env}.cloud.tateandlyle.com --root=/var/www/html/${site_name}/docroot"
 
 # Disable production only modules and configuration.
 drush8 pm-uninstall --yes tl_authentication_prod $multisite
@@ -27,4 +27,6 @@ drush8 pm-enable --yes tl_authentication_stage $multisite
 drush8 fr --yes tl_authentication_stage $multisite
 
 # Temporary change to help QA - ensure that the password for the service account is fixed.
-drush8 user-password svcCMSDrupal@tateandlyle.com --password="D9KUwqiQhswN4"
+drush8 user-password svcCMSDrupal@tateandlyle.com --password="D9KUwqiQhswN4" $multisite
+
+drush8 updatedb --yes $multisite
