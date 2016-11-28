666fe48e3d4227fab7c923cbc0df49e9101f02f4
Murtaza Alvi <smurtazama@gmail.com>
2016-11-29 00:18:29 +0530

    TLWeb-60: Responsive theme.
    Update the subtheme with gulp structure.
    Update the subtheme with linters to maintain coding standards.
    Add fonts, logo and basic configurations for the theme.
    Add tal_config module to automatically enable the new theme.
    Update ligntning.extend.yml to enable the module after installation.

---
+++ docroot/themes/custom/tateandlyle/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Contains Tate and Lyle Custom Javascript behaviours.
+ */
+
+/**
+ * Implements Drupal.behaviours.
+ */
+(function($, Drupal) {
+  "use strict";
+
+  Drupal.behaviors.tateandlyle = {
+    attach: function() {
+
+      // Put your code here.
+    }
+  };
+})(jQuery, Drupal);
