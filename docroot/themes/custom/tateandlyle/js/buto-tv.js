83ff6bff62857346b3dd4e38b8234942580ab906
Pradeep <pradeep@axelerant.com>
2016-11-28 23:17:44 +0530

    re-#tal-8 fixed the small changes updated by code sniffer.

--- docroot/themes/custom/tateandlyle/js/buto-tv.js
+++ docroot/themes/custom/tateandlyle/js/buto-tv.js
@@ -1,15 +1,24 @@
-//Pure HTML5 embed (note the `delivery` parameter that's passed into the player object)
-function buto_tv_player (d, conf) {
+/**
+ * @file
+ * Contains function for buto tv player. Pure HTML5 embed (note the `delivery` parameter that's passed into the player object)
+ */
+
+function buto_tv_player(d, conf) {
     var b = d.createElement('script');
     b.setAttribute('async', true);
     b.src = '//embed.buto.tv/js/' + conf.object_id;
     if (b.addEventListener) {
         b.addEventListener('load', function () {
-            if (window.buto) buto.addPlayer(conf);
+            if (window.buto) {
+                buto.addPlayer(conf);
+            }
         }, false);
-    } else if (b.readyState) {
+    }
+    else if (b.readyState) {
         b.onreadystatechange = function () {
-            if (window.buto) buto.addPlayer(conf);
+            if (window.buto) {
+                buto.addPlayer(conf);
+            }
         };
     }
     var s = d.getElementsByTagName('script')[0];
@@ -19,7 +28,7 @@ function buto_tv_player (d, conf) {
 
 Drupal.behaviors.talbutotv = {
     attach: function (context) {
-        jQuery.each(drupalSettings.tateandlyle, function( k, data ) {
+        jQuery.each(drupalSettings.tateandlyle, function (k, data) {
             buto_tv_player(context, {
                 object_id: data.videoid,
                 element_id: data.elementid,
