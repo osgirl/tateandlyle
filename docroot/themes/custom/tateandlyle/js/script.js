89828de0dced42ee56a1f4a72b6acce3cf378ecb
Aliya Khan <aliyakhan@Aliyas-MacBook-Air.local>
2017-01-05 11:02:22 +0530

    TAL-131 and 132 Image and Video Gallery theme

--- docroot/themes/custom/tateandlyle/js/script.js
+++ docroot/themes/custom/tateandlyle/js/script.js
@@ -8,7 +8,6 @@
  */
 (function ($, Drupal) {
   "use strict";
-
   Drupal.behaviors.collapse = {
     attach: function () {
       $(".in--country-read-more button").click(function () {
@@ -17,6 +16,30 @@
 
       $(".ingredient--submenu ul li").first().click(function () {
         $(".ingredient--submenu ul li").toggleClass("view");
+      });
+    }
+  };
+
+  Drupal.behaviors.carousel = {
+    attach: function () {
+      $("#carousel-example-generic").carousel({
+        interval: 4000
+      });
+
+      $(".carousel .item").each(function () {
+        var next = $(this).next();
+
+        if (!next.length) {
+          next = $(this).siblings(":first");
+        }
+        next.children(":first-child").clone().appendTo($(this));
+        for (var i = 0; i < 2; i++) {
+          next = next.next();
+          if (!next.length) {
+            next = $(this).siblings(":first");
+          }
+          next.children(":first-child").clone().appendTo($(this));
+        }
       });
     }
   };
