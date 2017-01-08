ca51b14b4ff9d63b384fe0e85e947b5d6bc017f3
Aliya Khan <aliyakhan@Aliyas-MacBook-Air.local>
2017-01-09 01:24:25 +0530

    TAL-88 and TAL-252 theming css fixes only

--- docroot/themes/custom/tateandlyle/build/script.js
+++ docroot/themes/custom/tateandlyle/build/script.js
@@ -44,6 +44,16 @@
     }
   };
 
+  Drupal.behaviors.toggleaccordion = {
+    attach: function () {
+      function toggleIcon(e) {
+        $(e.target).prev(".panel-heading").find(".more-less").toggleClass("glyphicon-plus glyphicon-minus");
+      }
+      $(".panel-group").on("hidden.bs.collapse", toggleIcon);
+      $(".panel-group").on("shown.bs.collapse", toggleIcon);
+    }
+  };
+
   Drupal.behaviors.scrollFix = {
     attach: function () {
       if ($(".ingredient--submenu").length) {
