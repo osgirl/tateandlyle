90abc1d9e1716ce077575729b5006b1f5d3834d1
Abhishek Dhariwal <abhishek@axelerant.com>
2017-01-11 12:18:56 +0530

    TAL-264: Images are now made explicitely downloadable.

--- docroot/modules/custom/tal_image_gallery/js/tal_image_gallery_download_image.js
+++ docroot/modules/custom/tal_image_gallery/js/tal_image_gallery_download_image.js
@@ -13,10 +13,12 @@
                 if ($('#cboxLoadedContent > img').attr('src')) {
                     var fullHref = $('#cboxLoadedContent > img').attr('src').replace(/styles\/large\/public\//,'');
                     var fullLink = $('<a/>');
+                    var filename =  fullHref.substr(fullHref.lastIndexOf("/")+1);
                     fullLink.attr('href', fullHref);
                     fullLink.attr('class', 'btn-primary download-image');
                     fullLink.attr('target', 'new');
                     fullLink.attr('title', Drupal.t('Right click to download'));
+                    fullLink.attr('download', filename);
                     fullLink.text(Drupal.t('Download'));
                     $('#cboxLoadedContent').append(fullLink);
                     $('#cboxLoadedContent').prepend($('#cboxTitle'));
