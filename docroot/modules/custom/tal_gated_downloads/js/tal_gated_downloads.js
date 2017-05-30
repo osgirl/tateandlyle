c9c166a8d3de4180b077223d4c585ec0218f714f
Abhishek Dhariwal <ad0811@gmail.com>
2017-05-30 11:52:21 +0530

    TAL-547: Some improvements to the file download process. (#609)
    * TAL-547: Added gated download functionality.
    
    * TAL-547: New message page added.
    
    * TAL-547: Gated download added to the related download blocks.
    
    * TAL-547: Gated download implemented on sap docs.
    
    * TAL-547: Formatting related issues fixed.
    
    * TAL-547: Formatting related issues fixed.
    
    * TAL-547: Formatting related issues fixed.
    
    * TAL-547: Formatting related issues fixed.
    
    * TAL-547: Formatting related issues fixed.
    
    * TAL-547: Some improvements to the file download process.
    
    * TAL-547: tal_gated_downloads module added as a dependency.
    
    * TAL-547: Code sniffer related issue fixed.

--- docroot/modules/custom/tal_gated_downloads/js/tal_gated_downloads.js
+++ docroot/modules/custom/tal_gated_downloads/js/tal_gated_downloads.js
@@ -2,12 +2,7 @@
     Drupal.behaviors.GatedDownloadsRedirect = {
         attach: function (context, settings) {
             $( document ).ready(function() {
-                var path = window.location.pathname;
-                var elements = [];
-                var fid = '';
-                elements = path.split('/');
-                fid = elements[elements.length - 1];
-                window.location.href = '/downloads/gated/file/' + fid;
+                window.location.href = '/downloads/gated/file';
             });
         }
     };
