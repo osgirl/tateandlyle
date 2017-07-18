12166cc26e44efa9e3cb40d1875306eadf231890
Amey Mudras <amey.mudras@acquia.com>
2017-07-18 16:55:22 +0530

    TALCD-41: Fixed the scroll issue.

--- docroot/modules/custom/tal_articles/js/tal_articles_post_list.js
+++ docroot/modules/custom/tal_articles/js/tal_articles_post_list.js
@@ -21,11 +21,8 @@
                 jQuery( document ).ajaxComplete(function() {
                     jQuery('html,body').animate({
                         scrollTop: jQuery(".tal-dynamic-post-wrapper #dynamic-post-list").offset().top - 140
-                    }, 'slow');
+                    }, 'slow', 'linear', stop());
                 });
-            });
-            jQuery(window).bind("mousewheel", function() {
-                jQuery("html, body").stop();
             });
         },
     };
