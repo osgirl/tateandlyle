151c1e97d851d0ab0f93bfd78eeb063e3ab65d4e
RahulSavaria <rahulsavaria@gmail.com>
2017-11-17 15:06:14 +0530

    TAL-60: Image downloads not working on image gallery.

--- docroot/modules/custom/tal_image_gallery/js/tal_image_gallery_download_image.js
+++ docroot/modules/custom/tal_image_gallery/js/tal_image_gallery_download_image.js
@@ -11,7 +11,8 @@
             $(context).bind('cbox_complete', function () {
                 // Add download link and move title.
                 if ($('#cboxLoadedContent > img').attr('src')) {
-                    var fullHref = $('#cboxLoadedContent > img').attr('src').replace(/styles\/large\/public\//,'');
+                    var fullHref = $('#cboxLoadedContent > img').attr('src').split('?')[0];
+                    fullHref = fullHref.replace(/\/styles(.+?)\/public/,'');
                     var fullLink = $('<a/>');
                     var filename =  fullHref.substr(fullHref.lastIndexOf("/")+1);
                     fullLink.attr('href', fullHref);
