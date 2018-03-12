d2e3be1ee3d3e169a1857d58b7752fb3b25a5f29
pratik.drake@gmail.com <pratik.drake@gmail.com>
2018-03-13 00:39:17 +0530

    TAL-138: Updated formatting.

--- docroot/modules/custom/tal_domain_redirect/src/TALDomainFileReaderFilter.php
+++ docroot/modules/custom/tal_domain_redirect/src/TALDomainFileReaderFilter.php
@@ -1,23 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pratikkamble
- * Date: 01/03/18
- * Time: 7:51 AM
- */
 
 namespace Drupal\tal_domain_redirect;
 
 use PhpOffice\PhpSpreadsheet\Reader\IReadFilter;
 
+/**
+ * Provides filter fo phpspreadsheet.
+ */
 
 class TALDomainFileReaderFilter implements IReadFilter {
 
-    public function readCell($column, $row, $worksheetName = '') {
-        if (in_array($column,range('A','C'))) {
-            return TRUE;
-        }
-        return FALSE;
+  /**
+   * {@inheritdoc}
+   */
+  public function readCell($column, $row, $worksheetName = '') {
+
+    if (in_array($column, range('A', 'C'))) {
+      return TRUE;
     }
+    return FALSE;
+  }
 
 }
