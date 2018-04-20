268ebb71e9e0c32b7c79edb9984f8028dd5af5ab
pratik.drake@gmail.com <pratik.drake@gmail.com>
2018-04-20 18:21:11 +0530

    TAL-147: PR chaanges added.

--- docroot/modules/custom/tal_webinar_gallery/src/Controller/WebinarGalleryController.php
+++ docroot/modules/custom/tal_webinar_gallery/src/Controller/WebinarGalleryController.php
@@ -3,45 +3,72 @@
 namespace Drupal\tal_webinar_gallery\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class WebinarGalleryController.
  */
 class WebinarGalleryController extends ControllerBase {
 
+  protected $entityTypeManager;
+
+  /**
+   * WebinarGalleryController constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManager $entityTypeManager
+   *   The EntityTypeManager service.
+   */
+  public function __construct(EntityTypeManager $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
   /**
    * Constructs a page with webinar gallary content.
+   *
+   * @return array
+   *   Webinar content grouped by categories.
    */
   public function content() {
 
-    $build = [
+    return [
       '#theme' => 'tal_webinar_gallery',
       '#categories' => $this->getCategoriesContent(),
     ];
 
-    return $build;
   }
 
   /**
-   * Returns an array of image carousel of all categories.
+   * Get images based on categories.
+   *
+   * @return array
+   *   Returns an array of image carousel of all categories.
    */
   public function getCategoriesContent() {
     $carousel = [];
 
     // Load all terms from media gallery vocabulary.
-    $terms = \Drupal::service('entity_type.manager')
+    $terms = $this->entityTypeManager
       ->getStorage("taxonomy_term")
       ->loadTree('webinar_category');
 
     foreach ($terms as $term) {
-      $termIds = [$term->tid];
       $carousel[] = [
         'name' => $term->name,
         'carousel' => [
           '#type' => 'view',
           '#name' => 'webinar_gallery',
           '#display_id' => 'block_1',
-          '#arguments' => $termIds,
+          '#arguments' => [$term->tid],
         ],
       ];
     }
